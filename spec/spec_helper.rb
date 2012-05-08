d945b5681dd86ee8f2b588d751a858e1b197f9ae
Junya Ogura <junyaogura@gmail.com>
2012-05-09 00:57:38 +0900

    Add fixture_content helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,18 @@ require 'smiling'
 
 RSpec.configure do |config|
 end
+
+# Public: Returns the String of specified file contents.
+#
+# filename - Fixture filename.
+#
+# Examples
+#
+#   stub_request(:get, 'ext.nicovideo.jp/api/getthumbinfo/sm9').
+#     to_return(fixture_content('getthumbinfo_sm9.txt'))
+#
+# Returns the String.
+def fixture_content(filename)
+  path = File.expand_path("../fixtures/#{filename}", __FILE__)
+  File.read(path)
+end
