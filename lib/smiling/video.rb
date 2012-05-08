81818059d1f8a7695f9dad99db1ce58c06d69308
Junya Ogura <junyaogura@gmail.com>
2012-05-08 22:52:45 +0900

    Implement video attributes.

--- lib/smiling/video.rb
+++ lib/smiling/video.rb
@@ -1,9 +1,56 @@
+require 'time'
+
 module Smiling
+  # Public: Video Represents a video of Nico Nico Douga.
   class Video
     attr_reader :id
+    attr_reader :title
+    attr_reader :description
+    attr_reader :thumbnail_url
+    attr_reader :first_retrieve
+    attr_reader :length
+    attr_reader :movie_type
+    attr_reader :size_high
+    attr_reader :size_low
+    attr_reader :view_counter
+    attr_reader :comment_num
+    attr_reader :mylist_counter
+    attr_reader :last_res_body
+    attr_reader :watch_url
+    attr_reader :type
+    attr_reader :embeddable
+    attr_reader :no_live_play
+    attr_reader :user_id
 
+    # Public: Create an instance of Video.
+    #
+    # hash - The hash parameters contains response of getthumbinfo API.
     def initialize(hash)
       @id = hash['nicovideo_thumb_response']['thumb']['video_id']
+      @title = hash['nicovideo_thumb_response']['thumb']['title']
+      @description = hash['nicovideo_thumb_response']['thumb']['description']
+      @thumbnail_url = hash['nicovideo_thumb_response']['thumb']['thumbnail_url']
+      @first_retrieve = Time.parse(hash['nicovideo_thumb_response']['thumb']['first_retrieve'])
+      @length = hash['nicovideo_thumb_response']['thumb']['length']
+      @movie_type = hash['nicovideo_thumb_response']['thumb']['movie_type']
+      @size_high = hash['nicovideo_thumb_response']['thumb']['size_high'].to_i
+      @size_low = hash['nicovideo_thumb_response']['thumb']['size_low'].to_i
+      @view_counter = hash['nicovideo_thumb_response']['thumb']['view_counter'].to_i
+      @comment_num = hash['nicovideo_thumb_response']['thumb']['comment_num'].to_i
+      @mylist_counter = hash['nicovideo_thumb_response']['thumb']['mylist_counter'].to_i
+      @last_res_body = hash['nicovideo_thumb_response']['thumb']['last_res_body']
+      @watch_url = hash['nicovideo_thumb_response']['thumb']['watch_url']
+      @type = hash['nicovideo_thumb_response']['thumb']['thumb_type']
+      @embeddable = hash['nicovideo_thumb_response']['thumb']['embeddable']
+      @no_live_play = hash['nicovideo_thumb_response']['thumb']['no_live_play']
+      @user_id = hash['nicovideo_thumb_response']['thumb']['user_id'].to_i
     end
   end
 end
+
+
+
+
+
+
+
--- spec/smiling_spec.rb
+++ spec/smiling_spec.rb
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
+require 'time'
 
 describe Smiling do
 
@@ -15,6 +17,51 @@ describe Smiling do
 
     subject { Smiling.video(video_id) }
     its(:id) { should eq(video_id) }
+    its(:title) { should eq('新・豪血寺一族 -煩悩解放 - レッツゴー！陰陽師') }
+    its(:description) { should eq('レッツゴー！陰陽師（フルコーラスバージョン）') }
+    its(:thumbnail_url) { should eq('http://tn-skr2.smilevideo.jp/smile?i=9') }
+    describe 'first_retrieve' do
+      subject { Smiling.video(video_id).first_retrieve }
+      it { should be_kind_of(Time) }
+      it { should eq(Time.parse('2007-03-06 00:33:00 +0900')) }
+    end
+    its(:length) { should eq('5:19') }
+    its(:movie_type) { should eq('flv') }
+    describe 'size_high' do
+      subject { Smiling.video(video_id).size_high }
+      it { should be_kind_of(Integer) }
+      it { should eq(21138631) }
+    end
+    describe 'size_low' do
+      subject { Smiling.video(video_id).size_low }
+      it { should be_kind_of(Integer) }
+      it { should eq(17436492) }
+    end
+    describe 'view_counter' do
+      subject { Smiling.video(video_id).view_counter }
+      it { should be_kind_of(Integer) }
+      it { should eq(9860861) }
+    end
+    describe 'comment_num' do
+      subject { Smiling.video(video_id).comment_num }
+      it { should be_kind_of(Integer) }
+      it { should eq(3996941) }
+    end
+    describe 'mylist_counter' do
+      subject { Smiling.video(video_id).mylist_counter }
+      it { should be_kind_of(Integer) }
+      it { should eq(110442) }
+    end
+    its(:last_res_body) { should eq('ギターソロ長門有希 憂ううううううううう ') }
+    its(:watch_url) { should eq('http://www.nicovideo.jp/watch/sm9') }
+    its(:type) { should eq('video') }
+    its(:embeddable) { should eq('1') }
+    its(:no_live_play) { should eq('0') }
+    describe 'user_id' do
+      subject { Smiling.video(video_id).user_id }
+      it { should be_kind_of(Integer) }
+      it { should eq(4) }
+    end
   end
 
 end
